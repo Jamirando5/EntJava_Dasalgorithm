45678d1c289a87c0f2387992a7fa7e31feedbe25
Jamirando5 <Jonlord333@gmail.com>
2023-10-25 20:26:20 +0800

    update

---
+++ MyWebApplication/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+/*script.js*/
+function handleSearch(event) {
+    if (event.key === "Enter") {
+        const searchQuery = event.target.value;
+        // Perform the search with the searchQuery
+        // You can add your search logic here
+        alert(`Searching for: ${searchQuery}`);
+    }
+}
